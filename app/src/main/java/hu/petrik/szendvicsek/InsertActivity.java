2165b4ea734f29fa63971346c2519885cb3c0c8e
Poós Dániel <poosdani@gmail.com>
2021-12-01 18:02:01 +0100

    Todo: saving and listing

--- app/src/main/java/hu/petrik/szendvicsek/InsertActivity.java
+++ app/src/main/java/hu/petrik/szendvicsek/InsertActivity.java
@@ -2,13 +2,36 @@ package hu.petrik.szendvicsek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InsertActivity extends AppCompatActivity {
-
+    private Button backToMain, save;
+    private EditText name, detail, makingT, prise
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        init();
+        backToMain.setOnClickListener(v->{
+            Intent toMain = new Intent(this, MainActivity.class);
+            startActivity(toMain);
+            finish();
+        });
+        save.setOnClickListener(v->{
+            if (!(name.getText().toString().isEmpty()&&prise.getText().toString().isEmpty()&&detail.getText().toString().isEmpty()&&makingT.getText().toString().isEmpty())){
+
+            }
+        });
+    }
+    public void init(){
+        name = findViewById(R.id.nameET);
+        detail = findViewById(R.id.detailsET);
+        makingT = findViewById(R.id.makingET);
+        prise = findViewById(R.id.priceET);
+        backToMain = findViewById(R.id.backFromInsert);
+        save = findViewById(R.id.saveBtn);
     }
 }
--- app/src/main/java/hu/petrik/szendvicsek/SearchResultActivity.java
+++ app/src/main/java/hu/petrik/szendvicsek/SearchResultActivity.java
@@ -2,13 +2,29 @@ package hu.petrik.szendvicsek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SearchResultActivity extends AppCompatActivity {
+    private Button backToMain;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        init();
+        backToMain.setOnClickListener(v->{
+            Intent toMain = new Intent(this,MainActivity.class);
+            startActivity(toMain);
+            finish();
+        });
+        //textview
+    }
+    public void init(){
+        textView = findViewById(R.id.list);
+        backToMain = findViewById(R.id.backFromSearch);
     }
 }
